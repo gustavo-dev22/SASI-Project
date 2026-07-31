b9933567f9b45fac7326ef9ab4d221900c857355
gustavo-dev22 <ingenierogustavo2017@gmail.com>
2026-07-30 21:47:48 -0500

    final

--- SASI.Infraestructura/Repositories/SasiService.cs
+++ SASI.Infraestructura/Repositories/SasiService.cs
@@ -23,7 +23,9 @@ namespace SASI.Infraestructura.Repositories
 
         public async Task<AccesosSasiResponseDto> ObtenerAccesosUsuario(string userName, string password)
         {
-            var endpoint = _configuration["SasiApi:BaseUrl"] + "/SASI/api/Auth/login"; // Ejemplo
+            var baseUrl = _configuration["SasiApi:BaseUrl"]?.TrimEnd('/');
+            var endpoint = $"{baseUrl}/SASI/api/Auth/login";
+
             var request = new { UserName = userName, Password = password };
 
             var response = await _httpClient.PostAsJsonAsync(endpoint, request);
@@ -34,14 +36,14 @@ namespace SASI.Infraestructura.Repositories
                 throw new Exception($"Error al obtener accesos desde SASI. Código: {response.StatusCode}. Detalles: {errorContent}");
             }
 
-
             var data = await response.Content.ReadFromJsonAsync<AccesosSasiResponseDto>();
             return data!;
         }
 
         public async Task<AccesosSasiResponseDto> ObtenerAccesosUsuario(string userName)
         {
-            var endpoint = _configuration["SasiApi:BaseUrl"] + $"/SASI/api/Auth/accesos-usuario/{userName}";
+            var baseUrl = _configuration["SasiApi:BaseUrl"]?.TrimEnd('/');
+            var endpoint = $"{baseUrl}/SASI/api/Auth/accesos-usuario/{userName}";
 
             var response = await _httpClient.GetAsync(endpoint);
 
